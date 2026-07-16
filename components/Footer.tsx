dd1592572ed121a9120c19dfd52b0dfbec64589b
Project Contributor <contributor@example.com>
2026-07-16 15:51:27 +0530

    Update developer portrait to use user's PNG image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -95,7 +95,7 @@ export default function Footer() {
               </p>
               <div className="flex items-center gap-3">
                 <img
-                  src="/images/developer-portrait.jpg"
+                  src="/images/developer-portrait.png"
                   alt="Developer"
                   className="w-10 h-10 rounded-full object-cover border border-white/20"
                 />
