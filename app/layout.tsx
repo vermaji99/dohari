35387076d6175490a2b1b53e9c9db9aa68ab6ef6
Project Contributor <contributor@example.com>
2026-07-16 11:59:46 +0530

    Add explicit robots meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🏞️</text></svg>" />
         <script
           type="application/ld+json"
